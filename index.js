d27b1770d3a9f8d68ee8d96747131cf092edeeff
slowrunr <mungaslov.nft@gmail.com>
2023-09-11 00:27:00 +0900

    added new classes

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
-fetch("https://dog.ceo/api/breeds/image/random")
-  .then((data) => data.json())
-  .then((res) => {
-    if (res.status !== "success") {
-      return;
-    }
+const addActionBtn = document.getElementById("addActionBtn");
+const actionOutput = document.getElementById("actionOutput");
 
-    const imgSrc = res.message;
-    document.querySelector(
-      ".js-img-from-api"
-    ).innerHTML = `<img src="${imgSrc}">`;
-  });
+function getActionFromApi() {
+  fetch("http://www.boredapi.com/api/activity")
+    .then((data) => data.json())
+    .then((res) => {
+      if (res.status !== "success") {
+        return null;
+      }
+      //const actionFromApi = res.activity;
+      console.log(res.activity);
+      //document.getElementById("actionOutput").innerText = `${actionFromApi}`;
+    });
+}
+
+addActionBtn.addEventListener("click", getActionFromApi);
+
+// fetch("https://dog.ceo/api/breeds/image/random")
+//   .then((data) => data.json())
+//   .then((res) => {
+//     if (res.status !== "success") {
+//       return;
+//     }
+//     const imgSrc = res.message;
+//     document.querySelector(
+//       ".js-img-from-api"
+//     ).innerHTML = `<img src="${imgSrc}">`;
+//   });
